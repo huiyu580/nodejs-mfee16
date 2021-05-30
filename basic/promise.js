1a2743cf06b5f84e4181085a8d6dfb2b0a1269c7
huiyulee <huiyu.lee580@gmail.com>
2021-05-30 09:07:34 +0800

    add file promise

---
+++ basic/promise.js
@@ -0,0 +1,40 @@
+
+let doWork = function (job, timer, cb) {
+    setTimeout(() => {
+      let dt = new Date();
+      cb(null, `完成工作: ${job} at ${dt.toISOString()}`);
+    }, timer);
+  };
+  
+// new Promise(function (resolve, reject) {});
+
+let doWorkPromise = function (job, timer, success){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let dt = new Date();
+            if (success){
+                return resolve(`完成工作: ${job} at ${dt.toISOString()}`)
+            }
+            reject(`工作失敗: ${job} at ${dt.toISOString()}`)
+        }, timer);
+    });
+};
+
+doWorkPromise("刷牙", 2000, true)
+    .then((result) => {
+        console.log(result);
+        return doWorkPromise("吃早餐", 3000, true)
+    })
+    .then((result) => {
+        console.log(result);
+        return doWorkPromise("寫功課", 3000,false)
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.error("發生錯誤", err);
+    })
+    .finally(() => {
+        console.log("我是finally")
+    })

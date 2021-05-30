b557ac30c0e0577aa123f5858330667d80231931
huiyulee <huiyu.lee580@gmail.com>
2021-05-30 16:02:40 +0800

    new file

---
+++ crawler/app-await.js
@@ -0,0 +1,66 @@
+const axios = require("axios");
+
+// TODO: 從 stock.txt 讀股票代碼進來
+// filesystem
+// npm i fs ??? -> 不用
+const fs = require("fs");
+// fs.readFile("stock.txt", "utf8", (err, data) => {
+//     if (err) {
+//         return console.error("讀檔錯誤", err);
+//     }
+//     console.log(`讀到的 stock code: ${data}`)
+// })
+
+let dt = new Date();
+// dt.toISOString().slice(0,10);
+let formatDate = dt.toISOString().slice(0,10).replace("-","").replace("-","");
+
+// 也可以使用moment js
+
+function readFilePromise () {
+    return new Promise((resolve, reject) => {
+        fs.readFile("stock.txt", "utf8", (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(data)
+        })
+    })
+};
+
+(async function(){
+    let stockCode = await readFilePromise();
+    let response = await axios({
+        method: 'get',
+        url: 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?',
+        params: {
+            date: formatDate,
+            stockNo: stockCode
+        }
+    })
+    if(response.data.stat === "OK"){
+        console.log(response.data.date);
+        console.log(response.data.title);
+    }
+})();
+
+// readFilePromise()
+//     .then((result) => {
+//         // console.log(result);
+//         return axios({
+//             method: 'get',
+//             url: 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?',
+//             params: {
+//                 date: formatDate,
+//                 stockNo: result
+//             }
+//         })
+//     })
+//     .then(function (response){
+//         if(response.data.stat === "OK"){
+//             console.log(response.data.date);
+//             console.log(response.data.title);
+//         }
+//     }).catch((err) => {
+//         console.log(err)
+//     })

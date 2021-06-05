2b35e628e13908df9233d0bf8547bd973fafa0b0
huiyulee <huiyu.lee580@gmail.com>
2021-06-05 11:24:00 +0800

    add bluebird

---
+++ crawler/app-bluebird.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+const fs = require("fs");
+let moment = require('moment');
+
+//覆蓋掉原生的promise
+const Promise = require("bluebird");
+// console.log(Promise)
+const readFileBlue = Promise.promisify(fs.readFile)
+
+readFileBlue("stock.txt", "utf-8")
+    .then((result) => {
+        // axios 是一個promise 當他return時，代表又丟了一個promise出去，所以外面可以再接一個then
+        return axios({
+            method: 'get',
+            url: 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?',
+            params: {
+                date: moment().format('YYYYMMDD'),
+                stockNo: result
+            }
+
+        })
+    })
+    // 這個then接的是axios這個promise的結果
+    .then(function (response){
+        if(response.data.stat === "OK"){
+            console.log(response.data.date);
+            console.log(response.data.title);
+        }
+    // 任何一個promise發生錯誤 readFilePromise 和 axios的錯誤都會被catch接住
+
+    }).catch((err) => {
+        console.log(err)
+    })

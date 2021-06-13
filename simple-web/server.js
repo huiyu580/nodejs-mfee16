77c39088431e05709cc590060c79d5380d511e94
Sandy Lee <75609720+huiyu580@users.noreply.github.com>
2021-06-13 13:45:29 +0800

    update content

--- simple-web/server.js
+++ simple-web/server.js
@@ -9,11 +9,11 @@ const http = require("http");
 // 建立server物件，還沒連線
 const server = http.createServer((req, res) => {
     console.log("有連線進來了");
-    console.log(req.url) //用來拿url
+    // console.log(req.url) //用來拿url
 
     // 將url一般化，移除他的query string，非必要結尾斜線，一律小寫
     const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLocaleLowerCase();
-
+    console.log(path)
     // 抓到localhost / IP
     const url = new URL(req.url, `http://${req.headers.host}`)
     // query string的參數變成物件
@@ -24,16 +24,19 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/plain;charset=UTF-8")
 
     switch(path) {
-        case "/":
-            let name = url.searchParams.get(name)
-            res.end(`hello ${name}，這是首頁`);
+        case "":
+            res.end("這是首頁");
             break;
         case "/test":
-            res.end("這是測試頁面");
+            let name = url.searchParams.get('name') || '網友'
+            res.end(`hello ${name}，這是首頁`);
             break;
         case "/about":
             res.end("關於我們");
             break;
+        default:
+            res.writeHead(404);
+            res.end("Not Found");
     }
 
 });

7f1142e1df4e74afc4892966b83c8c3dcb410e8e
Sandy Lee <75609720+huiyu580@users.noreply.github.com>
2021-06-13 16:58:14 +0800

    update content

--- simple-express/server.js
+++ simple-express/server.js
@@ -6,6 +6,24 @@ let app = express();
 
 // module < package < framework
 
-app.listen(3000, () => {
+// 使用中間件 middleware處理到router前的其他工作
+app.use(function(req, res, next){
+    let current = new Date();
+    console.log(`訪問時間:${current}`);
+    next();
+})
+
+// 路由
+app.get("/", (req, res) => {
+    res.send("Hello express")
+});
+app.get("/about", (res, req) => {
+    res.send("About express")
+})
+app.get("/test", (req, res) => {
+    res.send("Express test")
+})
+
+app.listen(3001, () => {
     console.log(`我跑起來了`)
 })

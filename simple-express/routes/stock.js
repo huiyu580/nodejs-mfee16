3e3d7a37a7b64790bdc45748c154f16bb3be655f
huiyulee <huiyu.lee580@gmail.com>
2021-06-19 14:00:03 +0800

    add h1

--- simple-express/routes/stock.js
+++ simple-express/routes/stock.js
@@ -10,6 +10,10 @@ router.get("/", async (req, res) => {
     })
 });
 router.get("/:stockCode", async (req, res) => {
+    let stockNameArr = await db.queryAsync('SELECT stock_name FROM stock WHERE stock_id = ?', [req.params.stockCode]); 
+    let stockName = stockNameArr[0].stock_name
+    console.log(stockName)
+
     const perPage = 10;
     let totalCountArr = await db.queryAsync('SELECT COUNT(*) as total FROM stock_price WHERE stock_id = ?', [req.params.stockCode]); 
     total = totalCountArr[0].total
@@ -18,13 +22,13 @@ router.get("/:stockCode", async (req, res) => {
     
 
     let priceResult = await db.queryAsync('SELECT * FROM stock_price WHERE stock_id = ? ORDER BY date  LIMIT ?,?', [req.params.stockCode, (currentPage-1)*perPage, perPage]);
-    console.log(priceResult)
+    // console.log(priceResult)
     res.render("stock/detail",{
         stockPrice : priceResult,
+        stockName,
         pagination:{
             lastPage,
-            currentPage,
-            
+            currentPage
         }
     })
 

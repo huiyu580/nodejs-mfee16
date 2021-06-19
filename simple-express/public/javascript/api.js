3284965e948890c643e72ad6be81c7894f8a4cde
huiyulee <huiyu.lee580@gmail.com>
2021-06-19 16:42:01 +0800

    add api.js

---
+++ simple-express/public/javascript/api.js
@@ -0,0 +1,17 @@
+// alert("dddd")
+// $.ajax({
+//     type: "get",
+//     url:"/api/stocks",
+//     datatype: "json"
+// }).done(function(data){
+//     console.log(data)
+// }).fail(function(){
+//     console.log(error)
+// }).always(function(){
+//     console.log("completed")
+// })
+
+axios.get('/api/stocks')
+.then(function (response) {
+    console.log(response);
+})
---
+++ simple-express/routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+const db = require("../utils/db.js")
+
+
+router.get("/stocks", async (req, res) => {
+    let result = await db.queryAsync('SELECT * FROM stock');
+    res.json(result)
+    // res.send(result)
+});
+
+module.exports = router;
